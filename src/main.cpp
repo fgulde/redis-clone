675be694dab59410d1fb6cf54fe5625c1148153f
Filip Gulde <f.gulde+github@proton.me>
2026-04-05 19:48:02 +0200

    Update to use asio

--- src/main.cpp
+++ src/main.cpp
@@ -1,74 +1,39 @@
 #include <iostream>
-#include <cstdlib>
 #include <string>
-#include <cstring>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <netdb.h>
-#include <vector>
+#include <asio.hpp>
+
+using asio::ip::tcp;
 
 int main() {
   // Flush after every std::cout / std::cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
-  
-  int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-  if (server_fd < 0) {
-   std::cerr << "Failed to create server socket\n";
-   return 1;
-  }
-  
-  // Since the tester restarts the program quite often, setting SO_REUSEADDR
-  // ensures that we don't run into 'Address already in use' errors
-  int reuse = 1;
-  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
-    std::cerr << "setsockopt failed\n";
-    return 1;
-  }
-  
-  sockaddr_in server_addr{};
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_addr.s_addr = INADDR_ANY;
-  server_addr.sin_port = htons(6379);
 
-  if (bind(server_fd, reinterpret_cast<sockaddr *>(&server_addr), sizeof(server_addr)) != 0) {
-    std::cerr << "Failed to bind to port 6379\n";
-    return 1;
-  }
+  // io_context coordinates all I/O-operations
+  asio::io_context io_context;
 
-  constexpr int connection_backlog = 5;
-  if (listen(server_fd, connection_backlog) != 0) {
-    std::cerr << "listen failed\n";
-    return 1;
-  }
-  
-  sockaddr_in client_addr{};
-  socklen_t client_addr_len = sizeof(client_addr);
+  // Acceptor opens the server socket, binds it and listens
+  // Port 6379 is standard for redis
+  tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), 6379));
+
   std::cout << "Waiting for a client to connect...\n";
-  std::cout << "Logs from your program will appear here!\n";
 
-  // Read from client
-  int client_fd = accept(server_fd, reinterpret_cast<sockaddr *>(&client_addr), &client_addr_len);
-  if (client_fd < 0) {
-    std::cerr << "accept failed\n";
-    return 1;
-  }
+  // Blocks until a client connects
+  tcp::socket socket(io_context);
+  acceptor.accept(socket);
   std::cout << "Client connected\n";
 
-  std::vector<char> buf(1024);
-  ssize_t bytes_read = read(client_fd, buf.data(), buf.size() - 1);
-  if (bytes_read > 0) {
-    std::string request ( buf.data(), bytes_read );
-    if (request.find("PING") != std::string::npos) {
-      const std::string response = "+PONG\r\n";
-      send(client_fd, response.c_str(), response.size(), 0);
-    }
-  }
+  asio::streambuf buf;
+  asio::read_until(socket, buf, "\r\n");
 
-  close(client_fd);
-  close(server_fd);
+  std::string request{
+    std::istreambuf_iterator<char>(&buf),
+    std::istreambuf_iterator<char>()
+  };
+
+  if (request.find("PING") != std::string::npos) {
+    asio::write(socket, asio::buffer(std::string("+PONG\r\n")));
+  }
 
   return 0;
 }
