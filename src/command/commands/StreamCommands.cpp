796a644b64a4c813353975fbfb43bf29aa8a3dd0
Filip Gulde <f.gulde+github@proton.me>
2026-04-30 16:03:59 +0200

    Add auto generation for Stream IDs

--- src/command/commands/StreamCommands.cpp
+++ src/command/commands/StreamCommands.cpp
@@ -16,12 +16,16 @@ void XAddCommand::execute(const Command& cmd, const asio::any_io_executor&,
   const std::string& key = cmd.args.at(0);
   const std::string& id = cmd.args.at(1);
 
+  // Parse fields
   std::vector<std::pair<std::string, std::string>> fields;
   for (size_t i = 2; i < cmd.args.size(); i += 2) {
     fields.emplace_back(cmd.args.at(i), cmd.args.at(i + 1));
   }
 
-  std::string new_id = store_.xadd(key, id, fields);
-
-  on_reply(std::format("${}\r\n{}\r\n", new_id.size(), new_id));
+  try {
+    std::string new_id = store_.xadd(key, id, fields);
+    on_reply(std::format("${}\r\n{}\r\n", new_id.size(), new_id));
+  } catch (const std::exception& e) {
+    on_reply(std::format("-ERR {}\r\n", e.what()));
+  }
 }
--- tests/integration/stream_test.cpp
+++ tests/integration/stream_test.cpp
@@ -28,6 +28,50 @@ TEST_F(StreamTest, XAddWrongArguments) {
     EXPECT_EQ(resp, "-ERR wrong number of arguments for XADD\r\n");
 }
 
+TEST_F(StreamTest, XAddAutoSequence) {
+    constexpr std::array<std::string_view, 5> args1 = {"XADD", "stream2", "1526919030474-*", "foo", "bar"};
+    const std::string resp1 = client.send_raw(client.encode_array(args1));
+    EXPECT_EQ(resp1, "$15\r\n1526919030474-0\r\n");
+
+    constexpr std::array<std::string_view, 5> args2 = {"XADD", "stream2", "1526919030474-*", "foo", "bar"};
+    const std::string resp2 = client.send_raw(client.encode_array(args2));
+    EXPECT_EQ(resp2, "$15\r\n1526919030474-1\r\n");
+}
+
+TEST_F(StreamTest, XAddAutoId) {
+    constexpr std::array<std::string_view, 5> args = {"XADD", "stream3", "*", "foo", "bar"};
+    const std::string resp = client.send_raw(client.encode_array(args));
+    EXPECT_TRUE(resp.starts_with("$"));
+    EXPECT_TRUE(resp.ends_with("\r\n"));
+    // It should be > 0-0 so there should be a '-'
+    EXPECT_NE(resp.find("-"), std::string::npos);
+}
+
+TEST_F(StreamTest, XAddInvalidIdSmaller) {
+    constexpr std::array<std::string_view, 5> args1 = {"XADD", "stream4", "100-1", "foo", "bar"};
+    client.send_raw(client.encode_array(args1));
+
+    constexpr std::array<std::string_view, 5> args2 = {"XADD", "stream4", "100-1", "foo", "bar"};
+    const std::string resp2 = client.send_raw(client.encode_array(args2));
+    EXPECT_EQ(resp2, "-ERR The ID specified in XADD is equal or smaller than the target stream top item\r\n");
+    
+    constexpr std::array<std::string_view, 5> args3 = {"XADD", "stream4", "99-1", "foo", "bar"};
+    const std::string resp3 = client.send_raw(client.encode_array(args3));
+    EXPECT_EQ(resp3, "-ERR The ID specified in XADD is equal or smaller than the target stream top item\r\n");
+}
+
+TEST_F(StreamTest, XAddZeroId) {
+    constexpr std::array<std::string_view, 5> args = {"XADD", "stream5", "0-0", "foo", "bar"};
+    const std::string resp = client.send_raw(client.encode_array(args));
+    EXPECT_EQ(resp, "-ERR The ID specified in XADD must be greater than 0-0\r\n");
+}
+
+TEST_F(StreamTest, XAddZeroTimeAutoSequence) {
+    constexpr std::array<std::string_view, 5> args = {"XADD", "stream6", "0-*", "foo", "bar"};
+    const std::string resp = client.send_raw(client.encode_array(args));
+    EXPECT_EQ(resp, "$3\r\n0-1\r\n");
+}
+
 TEST_F(StreamTest, XAddSetsTypeToStream) {
     constexpr std::array<std::string_view, 5> args = {"XADD", "s1", "0-1", "f", "v"};
     client.send_raw(client.encode_array(args));
