1bc618427f82e2992c0ca508dc5f578ceb2ea8d7
Filip Gulde <f.gulde+github@proton.me>
2026-05-14 03:17:28 +0200

    Edit shell files to use Ninja generator

--- run_tests.sh
+++ run_tests.sh
@@ -2,6 +2,6 @@
 set -e
 cd "$(dirname "$0")"
 
-cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE="${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake"
+cmake -B build -S . -G Ninja -DCMAKE_TOOLCHAIN_FILE="${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake"
 cmake --build ./build --target redis_tests
 cd build && ctest --output-on-failure
